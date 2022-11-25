e9831629ab9331979bbe932d94b5131dfa5e893f
Udesh Habaraduwa <udesh86@gmail.com>
2022-11-25 11:29:00 +0100

    Adds connection to database

--- gradio_ui/demo.py
+++ gradio_ui/demo.py
@@ -9,7 +9,8 @@ def predict(image):
     files = {"file": ("image.jpg", f)}
     pred = requests.post("http://ec2-3-88-219-227.compute-1.amazonaws.com/upload_image", files = files)
     # send the file to the database
-    r = requests.post("http://127.0.0.1:8000/upload_image", files=files)
+    r = requests.post("http://ec2-3-91-187-198.compute-1.amazonaws.com:8000/upload_image", files=files)
+    os.remove("image.jpg")
     return pred.text
 
 gr.Interface(predict, inputs=gr.inputs.Image(type="pil"), outputs="json").launch()
